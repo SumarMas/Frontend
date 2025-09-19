e373d0eea92fd165c972497cb4024a4082e61056
Camila Antonella Lopez <camilaal895@gmail.com>
2025-09-19 17:18:25 -0300

    update(home)

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../components/button-component/button-component";
+import { CollapseComponent } from "../../components/collapse-component/collapse-component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonComponent, CollapseComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+
+  cardInfo: { title: string, icon:string}[] = [
+    { title: 'Seguridad', icon: '🔒'},
+    { title: 'Transparencia', icon: '🔍'},
+    { title: 'Impacto Social', icon: '🌍'}
+  ]
 
 }
