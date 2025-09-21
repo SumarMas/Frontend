bd5d4486b79f773dd9ee2aa53c843afdd6919110
Camila Antonella Lopez <camilaal895@gmail.com>
2025-09-20 23:36:24 -0300

    update

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -10,10 +10,27 @@ import { CollapseComponent } from "../../components/collapse-component/collapse-
 })
 export class Home {
 
-  cardInfo: { title: string, icon:string}[] = [
-    { title: 'Seguridad', icon: '🔒'},
-    { title: 'Transparencia', icon: '🔍'},
-    { title: 'Impacto Social', icon: '🌍'}
+  cardInfo: { title: string, icon: string }[] = [
+    { title: 'Seguridad', icon: '🔒' },
+    { title: 'Transparencia', icon: '🔍' },
+    { title: 'Impacto Social', icon: '🌍' }
   ]
 
+  collapseInfo = [
+    {
+      title: '¿Cómo se verifican las ONGs?',
+      content: 'Las organizaciones deben registrar su información y subir documentación legal. Un administrador revisa y aprueba antes de que puedan crear campañas.',
+      check: false
+    },
+    {
+      title: '¿Puedo donar de forma anónima?',
+      content: 'Sí, al realizar tu donación puedes elegir no mostrar tu nombre públicamente en los comentarios de la campaña.',
+      check: false
+    },
+    {
+      title: '¿Qué sucede cuando una campaña alcanza su meta?',
+      content: 'La campaña se cierra automáticamente al cumplir la meta económica o al llegar a la fecha de finalización. La ONG puede luego solicitar el pago de los fondos recaudados.',
+      check: false
+    },
+  ];
 }
