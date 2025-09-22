129879c56332b726d5c1f029f53559312de8e04f
Camila Antonella Lopez <camilaal895@gmail.com>
2025-09-22 14:53:10 -0300

    update

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../components/button-component/button-component";
 import { CollapseComponent } from "../../components/collapse-component/collapse-component";
+import { StepsShowcaseComponent } from "../../components/steps-showcase-component/steps-showcase-component";
+
+type Step = { title: string; status: 'completed' | 'failed' | 'pending' };
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent, CollapseComponent],
+  imports: [ButtonComponent, CollapseComponent, StepsShowcaseComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
@@ -31,6 +34,31 @@ export class Home {
       title: '¿Qué sucede cuando una campaña alcanza su meta?',
       content: 'La campaña se cierra automáticamente al cumplir la meta económica o al llegar a la fecha de finalización. La ONG puede luego solicitar el pago de los fondos recaudados.',
       check: false
+    },
+  ];
+
+    steps= [
+    {
+      title: 'Crear cuenta',
+      subtitle: 'Donante u ONG',
+      body: 'Registrate gratis en Sumar+ con tu email y contraseña. Si tenés una ONG podés validarla luego.',
+      img: '/user-create.jpeg'
+    },
+    {
+      title: 'Verificar ONG',
+      subtitle: 'Solo organizaciones',
+      body: 'Subí la documentación necesaria en PDF o imágenes. Un administrador revisará y validará tu ONG.',
+      img: '/verify-documents.jpeg'
+    },
+    {
+      title: 'Publicar campaña',
+      body: 'Definí un título, meta económica, fecha de cierre, imágenes y etiquetas para difundir tu causa.',
+      img: '/publish-campaing.jpeg'
+    },
+    {
+      title: 'Recibir donaciones',
+      body: 'Comparte tu campaña. Los donantes podrán apoyarla de forma segura y transparente.',
+      img: '/donations.jpeg'
     },
   ];
 }
