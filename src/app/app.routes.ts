8d146ccfa131eec124746d98ad0da737753483f4
Camila Antonella Lopez <camilaal895@gmail.com>
2025-09-22 20:10:22 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,14 @@ import { Home } from './pages/home/home';
 export const routes: Routes = [
     { pathMatch: 'full', path: '', redirectTo: 'home' },
     { path: 'home', component: Home },
+    
+    // { path: 'login' },
+    // { path: 'register' },
+    // { path: 'campaigns' },
+    // { path: 'organizations' },
+    // { path: 'donations' },
+    // { path: 'dashboards' },
+
 
     //paginas de error
     { path: '401', component: ErrorPageComponent, data: { code: 401 } },
