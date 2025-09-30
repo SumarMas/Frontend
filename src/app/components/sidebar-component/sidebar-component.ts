a65f0161df6c8da5154bdeeb91f5f8b0e7fb143d
Camila Antonella Lopez <camilaal895@gmail.com>
2025-09-30 16:00:23 -0300

    feat(sidebar): add confirm dialog on logout

--- src/app/components/sidebar-component/sidebar-component.ts
+++ src/app/components/sidebar-component/sidebar-component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, input } from '@angular/core';
 import { Sidebuttons } from '../../models/nav-item';
 import { NgClass } from '@angular/common';
-import { SidebarService } from '../../services/sidebar-service';
+import { SidebarService } from '../../services/ui/sidebar-service';
 import { IconComponent } from "../icon-component/icon-component";
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
+import { ConfirmService } from '../../services/ui/confirm-service';
 
 type Role = 'ADMIN' | 'DONOR' | 'ORGANIZATION';
 
@@ -19,11 +20,23 @@ export class SidebarComponent {
   roles = input<Role[]>(['DONOR', 'ORGANIZATION']);
 
   sidebarService = inject(SidebarService);
-  authService = inject(AuthService);
+  authService = inject(AuthService)
+  confirmService = inject(ConfirmService);
   
   getButtonsForRoles(): typeof Sidebuttons {
   return this.buttons.filter(button =>
     button.roles.some(role => this.roles().includes(role))
   );
 }
+
+  async onLogout(){
+    const ok = await this.confirmService.ask('¿Estás seguro de que deseas cerrar sesión?', {
+    title: 'Cerrar sesión',
+    variant: 'warning',
+    confirmText: 'Cerrar sesión',
+    cancelText: 'Cancelar',
+  });
+  if (ok) { this.authService.logout() }
+  }
 }
+
