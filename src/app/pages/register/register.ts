675151d3f554ac8c045fff3e712f299a81a079a4
Camila Antonella Lopez <camilaal895@gmail.com>
2025-10-05 18:16:38 -0300

    remove(register): delete organization checkbox, fields and signals
    - Removed checkbox that allowed user + organization registration
    - Deleted unused form fields and related signals
    - Simplified register flow to only handle user creation

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -3,7 +3,7 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModu
 import { InputComponent } from "../../components/input-component/input-component";
 import { FormValidatorService } from '../../services/validations/form-validator-service';
 import { ButtonComponent } from "../../components/button-component/button-component";
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { PostUserDto } from '../../models/api/user';
 import { UserService } from '../../services/api/user-service';
 import { ToastService } from '../../services/ui/toast-service';
@@ -22,20 +22,15 @@ export class Register {
     username: FormControl<string | null>,
     password: FormControl<string | null>,
     confirmPassword: FormControl<string | null>,
-
-    //campos de organizacion se agregan dinámicamente segun el checkbox
-
-    phone?: FormControl<string | null>
   }>;
 
-  isOrganization = signal<boolean>(false);
   isLoading = signal(false);
 
   private fb = inject(FormBuilder);
   formValidator = inject(FormValidatorService);
   userService = inject(UserService);
   toastService = inject(ToastService);
-
+  router = inject(Router);
 
   constructor() {
     this.registerForm = this.fb.group({
@@ -54,49 +49,26 @@ export class Register {
           );
         }
       });
-
-    //agrega dinameicamente los campos de la organizaicon
-    effect(() => {
-      const phoneControl = new FormControl<string | null>('', [Validators.required, Validators.pattern(/^\+?[1-9]\d{1,14}$/)]);
-
-      if (this.isOrganization()) {
-        if (!this.registerForm.contains('phone')) {
-          this.registerForm.addControl('phone', phoneControl);
-        }
-      } else {
-        if (this.registerForm.contains('phone')) {
-          this.registerForm.removeControl('phone');
-        }
-      }
-    });
-  }
-
-  toggleOrganization() {
-    this.isOrganization.set(!this.isOrganization());
   }
 
   onSubmit() {
     if (this.registerForm.valid) {
       this.isLoading.set(true);
-      if (this.isOrganization()) {
-        //agregar logica de registro
-      }
-      else {
+
         const userData: PostUserDto = this.registerForm.value as PostUserDto;
         console.log('Registrando usuario:', userData);
         this.userService.register(userData).subscribe({
           next: (response) => {
             this.isLoading.set(false);
-            this.toastService.open('Usuario registrado con éxito', 'success', 3000, 'bottom-right');
+            this.toastService.open('Cuenta creada con éxito', 'success', 3000, 'bottom-right');
             this.registerForm.reset();
-            this.isOrganization.set(false);
+            this.router.navigate(['/login']);
           },
           error: (err) => {
             this.isLoading.set(false);
-            this.toastService.open('Error al registrar usuario', 'error', 3000, 'bottom-right');
+            this.toastService.open('Error al crear cuenta', 'error', 3000, 'bottom-right');
           }
         })
-      }
     }
   }
 }
