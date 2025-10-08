c774d39da32a3132e43ad40c701192e9454cd934
Camila Antonella Lopez <camilaal895@gmail.com>
2025-10-08 14:28:29 -0300

    refactor(user-models): rename 'name' and 'lastname' to 'firstname' and 'lastName'

--- src/app/models/api/user.ts
+++ src/app/models/api/user.ts
@@ -1,20 +1,20 @@
 export interface GetUserDto {
     id: string
-    name: string;
-    lastname: string;
+    firstName: string;
+    lastName: string;
     email: string;
     username: string;
 }
 
 export interface PutUserDto {
-    name?: string;
-    lastname?: string;
+    firstName?: string;
+    lastName?: string;
     profileFileId?: string;
 }
 
 export interface PostUserDto {
-    firstname: string;
-    lastname: string;
+    firstName: string;
+    lastName: string;
     email: string;
     username: string;
     password: string;
