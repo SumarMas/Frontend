66d918cbab135a81db1405c4aff9ffb6e05e8400
Camila Antonella Lopez <camilaal895@gmail.com>
2025-10-22 15:55:55 -0300

    feat(campaign-card): add method to calculate and show the result in the card restant days

--- src/app/components/campaign-card/campaign-card.ts
+++ src/app/components/campaign-card/campaign-card.ts
@@ -2,14 +2,24 @@ import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../button-component/button-component';
 import { GetCampaignDto } from '../../models/api/campaign';
 import { TruncatePipe } from '../../pipes/truncate-pipe';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-campaign-card',
-  imports: [ButtonComponent, TruncatePipe, CurrencyPipe],
+  imports: [ButtonComponent, TruncatePipe, CurrencyPipe, NgClass],
   templateUrl: './campaign-card.html',
   styleUrl: './campaign-card.scss'
 })
 export class CampaignCard {
   @Input() campaign: GetCampaignDto | null = null;
+
+  daysLeft(endDate: Date | undefined): number | string {
+    if (!endDate) {
+      return 'N/A';
+    }
+    const currentDate = new Date();
+    const timeDiff = new Date(endDate).getTime() - currentDate.getTime();
+    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return daysDiff >= 0 ? daysDiff : '';
+  }
 }
