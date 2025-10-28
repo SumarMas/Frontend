fbe12c4b960c3955cc3ceb458256976d648b7cc8
Camila Antonella Lopez <camilaal895@gmail.com>
2025-10-27 22:10:33 -0300

    feat(campaign-card): add navigation method

--- src/app/components/campaign-card/campaign-card.ts
+++ src/app/components/campaign-card/campaign-card.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ButtonComponent } from '../button-component/button-component';
 import { GetCampaignDto } from '../../models/api/campaign';
 import { TruncatePipe } from '../../pipes/truncate-pipe';
 import { CurrencyPipe, NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-campaign-card',
@@ -11,6 +12,8 @@ import { CurrencyPipe, NgClass } from '@angular/common';
   styleUrl: './campaign-card.scss'
 })
 export class CampaignCard {
+  private router = inject(Router);
+
   @Input() campaign: GetCampaignDto | null = null;
 
   daysLeft(endDate: Date | undefined): number | string {
@@ -21,5 +24,9 @@ export class CampaignCard {
     const timeDiff = new Date(endDate).getTime() - currentDate.getTime();
     const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
     return daysDiff >= 0 ? daysDiff : '';
+  }
+
+  navigate(){
+    this.router.navigate(['/organizations/1/campaign', this.campaign?.id]);
   }
 }
